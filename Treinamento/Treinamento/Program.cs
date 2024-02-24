9622fb6adf9ec4722316947591a1632c6e1f3b24
Jhou1607 <160132458+Jhou1607@users.noreply.github.com>
2024-02-24 19:16:38 -0300

    Add files via upload

---
+++ Treinamento/Treinamento/Program.cs
@@ -0,0 +1,17 @@
+﻿double[] salario = new double[10];
+float maior = 0;
+for (int i = 0; i < 10; i++)
+{
+     for (int j = 0; j < 10; j++)
+     {
+      Console.WriteLine("Digite o salario " + (j + 1) + "");
+      salario[i] = double.Parse(Console.ReadLine());    
+
+          if (salario[i] > maior)
+          {
+                maior = (int)salario[i];
+           }
+            
+      }Console.WriteLine("O maior salário é: " + maior);
+    break;
+}
